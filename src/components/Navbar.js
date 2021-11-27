a42362e9724c595fd47e9c4e34862459175ccf5e
priyajoshipj <79351439+priyajoshipj@users.noreply.github.com>
2021-11-27 22:31:49 +0530

    minnor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://blogs.soumyajit.tech/"
+                href="#"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="#"
                 target="_blank"
                 className="fork-btn-inner"
               >
